d6aacfc8f5842546e8e545bccfb7b254af340761
Mohamed Saber <msaber.shi@outlook.com>
2020-06-21 14:17:25 +0200

    Creation Component et interaction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'newapp';
+  public multiplier(nbr: number): number 
+  {
+    return nbr*nbr;
+  };
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NouveauComponent } from './theme/nouveau/nouveau.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NouveauComponent
   ],
   imports: [
     BrowserModule
