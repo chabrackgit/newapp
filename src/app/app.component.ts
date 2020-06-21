635c8a70c9ca2e507ed7c400804eda3ced89c971
Mohamed Saber <msaber.shi@outlook.com>
2020-06-21 16:10:53 +0200

    ngStyle interaction avec la forme et le style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
 
-  public displayButton = true;
+    public color = 'green';
+    public hauteur = 200 + "px";
+    public largeur = 200 + "px";
+
 }
